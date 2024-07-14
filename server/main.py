f90aaba226210bdd5bd0c5cce840a6c73adc5520
lscblack <l.christian@alustudent.com>
2024-07-14 13:58:05 +0200

    changed confingurations

--- server/main.py
+++ server/main.py
@@ -4,7 +4,10 @@ from typing import Optional
 from Endpoints import auth, apis
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="AfiaCare Api Documentaion",  # Replace with your desired title
+    description="Afiacare aims to revolutionize healthcare management in Cameroon by providing a comprehensive digital platform. This platform will address common issues like data mismanagement, donor matching, and information sharing between hospitals. By leveraging technology, Afiacare will enhance the accuracy, efficiency, and accessibility of healthcare services.",
+)
 
 # Include the routers from auth and apis
 app.include_router(auth.router)
