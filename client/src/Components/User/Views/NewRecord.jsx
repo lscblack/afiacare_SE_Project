52c6b8d0886a173efceb3262c0b80c2c21be73b4
lscblack <l.christian@alustudent.com>
2024-07-30 22:17:44 +0200

    done with user Setup

---
+++ client/src/Components/User/Views/NewRecord.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function NewRecord() {
+  return (
+    <div className=' p-6'>
+        <div className="bg-white p-2 rounded-lg">
+
+        </div>
+    </div>
+  )
+}
