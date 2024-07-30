c10a0bb92c9414973e8b256c72b00cb9169ae111
lscblack <l.christian@alustudent.com>
2024-07-30 22:24:52 +0200

    done with documentation

--- client/src/Components/User/Views/NewAppointment.jsx
+++ client/src/Components/User/Views/NewAppointment.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function NewAppointment() {
   return (
-    <div>
-        
+    <div className=' p-6'>
+        <div className="bg-white p-2 rounded-lg">
+
+        </div>
     </div>
   )
 }
